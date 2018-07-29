66e4015e1da767d63829b7c652d037c75f4a42f3
Natalia Harateh <nambatee@users.noreply.github.com>
2018-07-29 19:48:44 -0400

    remove mysql

--- js/lib/index.js
+++ js/lib/index.js
@@ -2,7 +2,6 @@
 
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
-import mysql from 'mysql'
 import config from '../config'
 
 let app = express()
@@ -23,27 +22,9 @@ app.listen(port, (err) => {
 
 app.get(`/${config.toolname}`, (req, res) => { res.send('🤓') })
 
-let pool = mysql.createPool({
-    host: config.host,
-    user: config.user,
-    password: config.password,
-    database: config.database
-})
-
 app.post('/', (req, res) => {
     let payload = req.body
-
-    let teamID = payload.team_id
-
-    pool.query(`select * from config.table where team_id = ${teamID}`, (err, results, fields) => {
-        if (err) {
-            throw err
-        } else {
-            console.log(results)
-            console.log(fields)
-        }
-    })
-
+    
     if (!payload || payload.token !== 'config') {
         let err = `✋ Wiki—what? An invalid slash token was provided.
         Is your Slack slash token correctly configured?`
