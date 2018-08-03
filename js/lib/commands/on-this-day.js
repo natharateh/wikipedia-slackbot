d172c7793e0cb7496abfd71ddc1b6c85192592bc
Natalia Harateh <nambatee@users.noreply.github.com>
2018-08-02 22:19:20 -0400

    update on this day

--- js/lib/commands/on-this-day.js
+++ js/lib/commands/on-this-day.js
@@ -3,7 +3,7 @@
 /* eslint-disable camelcase */
 
 import request from 'request-promise-native'
-import { feed, WIKIPEDIA_BASE_URL } from './helpers/endpoints'
+import { feed } from './helpers/endpoints'
 import { today } from './helpers/date'
 import message from '../message-defaults'
 
@@ -19,18 +19,19 @@ const handler = (payload, response) => {
         let selected = object.selected
         let event = selected[Math.floor(Math.random() * selected.length)]
         let pages = event.pages
-        let text = event.text
         let random = Math.floor(Math.random() * pages.length)
-        let title = pages[random].normalizedtitle
-        let summary = pages[random].extract
+        let page = pages[random]
+        let title = page.titles.normalized
+        let title_link = page.content_urls.desktop.page
+        let text = page.extract
         let years_ago = `🗓 ${today.year - event.year} years ago: ${text}`
 
         let attachments = [
             {
                 pretext: years_ago,
                 title,
-                title_link: WIKIPEDIA_BASE_URL + title,
-                text: summary,
+                title_link,
+                text,
                 color: '#3366cc'
             }
         ]
