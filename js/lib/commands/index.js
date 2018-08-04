440adf71f7693398e642139565eb72dece2c80ef
Natalia Harateh <nambatee@users.noreply.github.com>
2018-08-03 21:11:59 -0400

    add top read

--- js/lib/commands/index.js
+++ js/lib/commands/index.js
@@ -4,10 +4,12 @@ import help from './help'
 import featured from './featured'
 import onThisDay from './on-this-day'
 import random from './random'
+import topRead from './topread'
 
 export default [
     help,
     featured,
     onThisDay,
-    random
+    random,
+    topRead
 ]
