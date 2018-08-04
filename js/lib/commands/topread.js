808761798e0a21ab0c253bc0d06b56f28e2ecd65
Natalia Harateh <nambatee@users.noreply.github.com>
2018-08-03 21:13:48 -0400

    add top read 🤷

---
+++ js/lib/commands/topread.js
@@ -0,0 +1,43 @@
+'use strict'
+
+/* eslint-disable camelcase */
+
+import request from 'request-promise-native'
+import { feed } from './helpers/endpoints'
+import message from '../message-defaults'
+
+const handler = (payload, response) => {
+
+    let options = {
+        uri: feed.FEATURED,
+        json: true
+    }
+
+    request(options).then((object) => {
+  
+        let articles = object.mostread.articles
+        let [article] = articles
+        let title = article.displaytitle
+        let title_link = article.content_urls.desktop.page
+        let text = article.extract
+
+        let attachments = [
+            {
+                pretext: 'Top read today 📈',
+                title,
+                title_link,
+                text,
+                color: '#3366cc'
+            }
+        ]
+  
+        response.set('content-type', 'application/json')
+        response.status(200).json(message(payload, attachments))  
+    })
+  
+}
+
+export default {
+    pattern: /top\sread/ig,
+    handler
+}
