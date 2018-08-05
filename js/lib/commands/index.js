340c7ef0611159b2cba2bed41621b38ff95702fe
Natalia Harateh <nambatee@users.noreply.github.com>
2018-08-05 12:04:08 -0400

    export search

--- js/lib/commands/index.js
+++ js/lib/commands/index.js
@@ -5,11 +5,13 @@ import featured from './featured'
 import onThisDay from './on-this-day'
 import random from './random'
 import topRead from './topread'
+import search from './search'
 
 export default [
     help,
     featured,
     onThisDay,
     random,
-    topRead
+    topRead,
+    search
 ]
