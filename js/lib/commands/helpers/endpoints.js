db250d3decfef61218c21ce370fa675fb6a6d850
Natalia Harateh <nambatee@users.noreply.github.com>
2018-08-07 21:38:54 -0400

    use urls, not strings

--- js/lib/commands/helpers/endpoints.js
+++ js/lib/commands/helpers/endpoints.js
@@ -2,20 +2,20 @@
 
 import { date } from './date'
 
-export const WIKIPEDIA_BASE_URL = 'https://en.wikipedia.org/'
+export const WIKIPEDIA_BASE_URL = new URL('https://en.wikipedia.org/')
 
-const API_URL = `${WIKIPEDIA_BASE_URL}api/rest_v1/`
+const API_URL = new URL('api/rest_v1/', WIKIPEDIA_BASE_URL)
 
-const FEED_URL = `${API_URL}feed/`
+const FEED_URL = new URL('feed/', API_URL.href)
 
-export const RANDOM_URL = `${API_URL}page/random/summary`
+export const RANDOM_URL = new URL('page/random/summary', API_URL.href)
 
-export const pageSummaryURL = (title) => `${API_URL}page/summary/${title}`
+export const pageSummaryURL = (title) => new URL(`page/summary/${title}`, API_URL.href)
 
-export const searchURL = (searchTerm) => `${WIKIPEDIA_BASE_URL}w/api.php?action=query&list=prefixsearch&format=json&pslimit=1&psnamespace=0&pssearch=${searchTerm}`
+export const searchURL = (searchTerm) => new URL(`w/api.php?action=query&list=prefixsearch&format=json&pslimit=1&psnamespace=0&pssearch=${searchTerm}`, WIKIPEDIA_BASE_URL.href)
 
 export const feed = {
-    FEATURED_TODAY: `${FEED_URL}featured/${date.today.yearMonthDay}`,
-    FEATURED_YESTERDAY: `${FEED_URL}featured/${date.yesterday.yearMonthDay}`,
-    ON_THIS_DAY: `${FEED_URL}onthisday/selected/${date.today.monthDay}`
+    FEATURED_TODAY: new URL(`featured/${date.today.yearMonthDay}`, FEED_URL.href),
+    FEATURED_YESTERDAY: new URL(`featured/${date.yesterday.yearMonthDay}`, FEED_URL.href),
+    ON_THIS_DAY: new URL(`onthisday/selected/${date.today.monthDay}`, FEED_URL.href)
 }
