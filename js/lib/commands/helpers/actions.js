15b36913deb01697b731f1f7447d38d2d4ff8fe5
Natalia Harateh <nambatee@users.noreply.github.com>
2018-09-17 09:03:25 -0400

    add actions helper

---
+++ js/lib/commands/helpers/actions.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const send = {
+    name: 'send',
+    text: 'Send',
+    type: 'button',
+    value: 'send'
+}
+
+const cancel = {
+    name: 'cancel',
+    text: 'Cancel',
+    style: 'danger',
+    type: 'button',
+    value: 'cancel'
+}
+
+export default [
+    send,
+    cancel
+]
