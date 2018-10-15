962fd2be4e23c152ce038742b32e538f8b7c0651
Natalia Harateh <nambatee@users.noreply.github.com>
2018-10-15 11:44:41 -0400

    remove keys from redis after 24h

--- js/lib/commands/helpers/original-message.js
+++ js/lib/commands/helpers/original-message.js
@@ -4,6 +4,9 @@
 
 import { redisClient, redisPrefix } from '../..'
 
+// 24 hours
+const expiryTimeInSeconds = 86400
+
 export function saveOriginalMessage(payload, callbackID, originalMessage) {
     let teamID = payload.team_id
     let channelID = payload.channel_id
@@ -12,7 +15,9 @@ export function saveOriginalMessage(payload, callbackID, originalMessage) {
 
     let key = redisKey(teamID, channelID, userID, token, callbackID)
 
-    redisClient.set(key, JSON.stringify(originalMessage))
+    redisClient.set(key, JSON.stringify(originalMessage), 'EX', expiryTimeInSeconds)
 }
 
 export const redisKey = (teamID, channelID, userID, token, callbackID) => `${redisPrefix}-${teamID}-${channelID}-${userID}-${token}-${callbackID}`
+
+export const expiryTimeInHours = expiryTimeInSeconds / 3600
