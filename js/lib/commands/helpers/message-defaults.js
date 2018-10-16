5cbb07734f24b1394bd0d3907c351cb7df96db3b
Natalia Harateh <nambatee@users.noreply.github.com>
2018-10-16 19:22:39 -0400

    add attachments to message defaults

--- js/lib/commands/helpers/message-defaults.js
+++ js/lib/commands/helpers/message-defaults.js
@@ -1,9 +1,48 @@
 'use strict'
 
+import actions from './actions'
+
 /* eslint-disable camelcase */
 
-export default (payload, attachments) => ({
-    response_type: 'ephemeral',
-    channel: payload.channel_name,
+export const ResponseType = Object.freeze({
+    EPHEMERAL: 'ephemeral',
+    IN_CHANNEL: 'in_channel'
+})
+
+export const message = (responseType, attachments) => ({
+    response_type: responseType,
     attachments
 })
+
+export const articleCallbackID = (commandCallbackID, articleID) => `${commandCallbackID}-${articleID}`
+
+export const attachments = (article, pretext, color, callback_id) => {
+    let title = article.titles.normalized
+    let title_link = article.content_urls.desktop.page
+    let text = article.extract
+
+    let attachmentsWithActions = [
+        {
+            pretext,
+            title,
+            title_link,
+            text,
+            color,
+            callback_id,
+            actions
+        }
+    ]
+
+    let attachmentsWithoutActions = {
+        pretext,
+        title,
+        title_link,
+        text,
+        color
+    }
+
+    return {
+        withActions: attachmentsWithActions, 
+        withoutActions: attachmentsWithoutActions
+    }
+}
