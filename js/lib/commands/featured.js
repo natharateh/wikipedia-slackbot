8a5375eed139c5b6cbf6cf1f36c2ffe2f1d549c3
Natalia Harateh <nambatee@users.noreply.github.com>
2018-10-16 19:23:02 -0400

    use new defaults in featured

--- js/lib/commands/featured.js
+++ js/lib/commands/featured.js
@@ -4,14 +4,12 @@
 
 import request from 'request-promise-native'
 import { feed } from './helpers/endpoints'
-import { saveOriginalMessage } from './helpers/original-message'
-import message from './helpers/message-defaults'
+import { saveMessageAttachments } from './helpers/saving-message-attachments'
+import { articleCallbackID, attachments, message, ResponseType } from './helpers/message-defaults'
 import headers from './helpers/request-headers'
-import actions from './helpers/actions'
 import respondSafely from './helpers/safe-response'
 
-const handler = (payload, response) => {
-
+const getFeaturedArticle = new Promise((resolve, reject) => {
     let options = {
         uri: feed.FEATURED_TODAY,
         json: true,
@@ -19,44 +17,36 @@ const handler = (payload, response) => {
     }
 
     request(options).then((object) => {
-        // Respond with 200 right away to avoid timeout
-        response.status(200).end()
-
-        let responseURL = payload.response_url
-        
         let article = object.tfa
-        let title = article.titles.normalized
-        let title_link = article.content_urls.desktop.page
-        let text = article.extract
+
+        resolve(article)
+    }).
+    
+        catch((err) => {
+            reject(err)
+        })
+})
+
+const handler = (payload, response) => {
+    // Respond with 200 right away to avoid timeout
+    response.status(200).end()
+
+    getFeaturedArticle.then((article) => {
         let pretext = 'Featured article for today 💫'
         let color = '#FFCC33'
-        let page_id = article.pageid
-        let callback_id = `${payload.text}-${page_id}`
 
-        let attachments = [
-            {
-                pretext,
-                title,
-                title_link,
-                text,
-                color,
-                callback_id,
-                actions
-            }
-        ]
+        let articleID = article.pageid
 
-        const originalMessage = {
-            pretext,
-            title,
-            title_link,
-            text,
-            color 
-        }
+        let responseURL = payload.response_url
+        let commandCallbackID = payload.text
 
-        saveOriginalMessage(callback_id, originalMessage)
-        respondSafely(responseURL, message(payload, attachments))
+        let key = articleCallbackID(commandCallbackID, articleID)
+        let messageAttachments = attachments(article, pretext, color, key)
+        let responseMessage = message(ResponseType.EPHEMERAL, messageAttachments.withActions)
+        
+        saveMessageAttachments(key, messageAttachments.withoutActions)
+        respondSafely(responseURL, responseMessage)
     })
-  
 }
 
 export default {
