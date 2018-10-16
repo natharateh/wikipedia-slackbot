f68ad8346a2687c0ffb600e18b1c572c33a8a7bf
Natalia Harateh <nambatee@users.noreply.github.com>
2018-10-16 19:23:25 -0400

    use new defaults in on this day

--- js/lib/commands/on-this-day.js
+++ js/lib/commands/on-this-day.js
@@ -5,14 +5,12 @@
 import request from 'request-promise-native'
 import { feed } from './helpers/endpoints'
 import { today } from './helpers/date'
-import message from './helpers/message-defaults'
 import headers from './helpers/request-headers'
-import actions from './helpers/actions'
-import { saveOriginalMessage } from './helpers/original-message'
+import { articleCallbackID, attachments, message, ResponseType } from './helpers/message-defaults'
+import { saveMessageAttachments } from './helpers/saving-message-attachments'
 import respondSafely from './helpers/safe-response'
 
-const handler = (payload, response) => {
-
+const getOnThisDayEvent = new Promise((resolve, reject) => {
     let options = {
         uri: feed.ON_THIS_DAY,
         json: true,
@@ -20,49 +18,47 @@ const handler = (payload, response) => {
     }
 
     request(options).then((object) => {
-        // Respond with 200 right away to avoid timeout
-        response.status(200).end()
-
-        let responseURL = payload.response_url
-
         let selected = object.selected
         let event = selected[Math.floor(Math.random() * selected.length)]
-        let pages = event.pages
-        let random = Math.floor(Math.random() * pages.length)
-        let page = pages[random]
-        let title = page.titles.normalized
-        let title_link = page.content_urls.desktop.page
-        let text = page.extract
+
+        resolve(event)
+    }).
+    
+        catch((err) => {
+            reject(err)
+        })
+})
+
+const onThisDayArticle = (event) => {
+    let pages = event.pages
+    let random = Math.floor(Math.random() * pages.length)
+    let article = pages[random]
+
+    return article
+}
+
+const handler = (payload, response) => {
+    // Respond with 200 right away to avoid timeout
+    response.status(200).end()
+
+    getOnThisDayEvent.then((event) => {
         let years_ago = `🗓 ${today.year - event.year} years ago: ${event.text}`
         let pretext = years_ago
         let color = '#3366cc'
-        let page_id = page.pageid
-        let callback_id = `${payload.text}-${page_id}`
 
-        let attachments = [
-            {
-                pretext,
-                title,
-                title_link,
-                text,
-                color,
-                callback_id,
-                actions
-            }
-        ]
+        let article = onThisDayArticle(event)
+        let articleID = article.pageid
+        
+        let responseURL = payload.response_url
+        let commandCallbackID = payload.text
 
-        const originalMessage = {
-            pretext,
-            title,
-            title_link,
-            text,
-            color 
-        }
+        let key = articleCallbackID(commandCallbackID, articleID)
+        let messageAttachments = attachments(article, pretext, color, key)
+        let responseMessage = message(ResponseType.EPHEMERAL, messageAttachments.withActions)
 
-        saveOriginalMessage(callback_id, originalMessage)
-        respondSafely(responseURL, message(payload, attachments))
+        saveMessageAttachments(key, messageAttachments.withoutActions)
+        respondSafely(responseURL, responseMessage)
     })
-
 }
 
 export default {
