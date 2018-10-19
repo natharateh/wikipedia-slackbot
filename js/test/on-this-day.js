b1a13025c357c80fc2eb79312ac5e5ae3e3b7e3f
Natalia Harateh <nambatee@users.noreply.github.com>
2018-10-19 07:37:55 -0400

    update on this day tests

--- js/test/on-this-day.js
+++ js/test/on-this-day.js
@@ -9,75 +9,179 @@ import chai from 'chai'
 import 'chai/register-expect'
 import chaiHttp from 'chai-http'
 import { app, path } from '../lib/index'
+import { testHelper } from '../lib/commands/on-this-day'
+
+import { testMessageAttachmentsWithoutActionsShouldHaveMatchingProperties,
+    testMessageAttachmentsWithActionsShouldHaveMatchingProperties,
+    testCancelAction, testSendAction } from './common'
 
 chai.use(chaiHttp)
 
 describe('/on this day', () => {
+    const articleID = '123'
+    const color = testHelper.color
+    const command = 'on this day'
+    const articleKey = testHelper.articleKey(command, articleID)
+
     it('should respond with status 200', done => {
         chai.request(app)
             .post(path)
-            .send({ text: 'on this day',
+            .send({ text: command,
                 token: 'sampletoken' })
             .end((err, res) => {
                 expect(err).to.be.null
                 expect(res).to.have.status(200)
+
                 done()
             })
     })
 
-    it('should have pretext', done => {
-        chai.request(app)
-            .post(path)
-            .send({ text: 'on this day',
-                token: 'sampletoken' })
-            .end((_, res) => {
-                let text = JSON.parse(res.text)
-                let [attachment] = text.attachments
+    it('should get event', done => {
+        testHelper.getOnThisDayEvent.then((event) => {
+            expect(event).to.not.be.null
 
-                expect(attachment.pretext).to.not.be.empty
-                done()
-            })
+            done()
+        })
     })
 
-    it('should have title', done => {
-        chai.request(app)
-            .post(path)
-            .send({ text: 'on this day',
-                token: 'sampletoken' })
-            .end((_, res) => {
-                let text = JSON.parse(res.text)
-                let [attachment] = text.attachments
+    it('should get event with year', done => {
+        testHelper.getOnThisDayEvent.then((event) => {
+            expect(event.year).to.not.be.null
 
-                expect(attachment.title).to.not.be.empty
-                done()
-            })
+            done()
+        })
     })
 
-    it('should have title link', done => {
-        chai.request(app)
-            .post(path)
-            .send({ text: 'on this day',
-                token: 'sampletoken' })
-            .end((_, res) => {
-                let text = JSON.parse(res.text)
-                let [attachment] = text.attachments
+    it('should get event with text', done => {
+        testHelper.getOnThisDayEvent.then((event) => {
+            expect(event.text).to.not.be.null
 
-                expect(attachment.title_link).to.not.be.empty
-                done()
-            })
+            done()
+        })
     })
 
-    it('should have text', done => {
-        chai.request(app)
-            .post(path)
-            .send({ text: 'on this day',
-                token: 'sampletoken' })
-            .end((_, res) => {
-                let text = JSON.parse(res.text)
-                let [attachment] = text.attachments
+    it('should get article', done => {
+        testHelper.getOnThisDayEvent.then((event) => {
+            const article = testHelper.onThisDayArticle(event)
+            
+            expect(article).to.not.be.null
 
-                expect(attachment.text).to.not.be.empty
-                done()
-            })
+            done()
+        })
+    })
+
+    it('should get article with id', done => {
+        testHelper.getOnThisDayEvent.then((event) => {
+            const article = testHelper.onThisDayArticle(event)
+            const articleID = article.pageid
+
+            expect(articleID).to.not.be.null
+
+            done()
+        })
+    })
+
+    it('should get pretext', done => {
+        testHelper.getOnThisDayEvent.then((event) => {
+            const pretext = testHelper.pretext(testHelper.today, event)
+
+            expect(pretext).to.not.be.null
+            done()
+        })
+    })
+
+    it('should get color', done => {
+        expect(color).to.not.be.null
+        done()
+    })
+
+    it('should get matching article key', done => {
+        expect(articleKey).to.eql(`${command}-${articleID}`)
+        done()
+    })
+
+    it('should create message attachments with actions', done => {
+        testHelper.getOnThisDayEvent.then((event) => {
+            const article = testHelper.onThisDayArticle(event)
+            const pretext = testHelper.pretext(testHelper.today, event)
+            const attachments = testHelper.attachments(article, pretext, color, articleKey)
+            const withActions = attachments.withActions
+
+            expect(withActions).to.not.be.null
+
+            done()
+        })
+    })
+
+    it('should create message attachments with send action', done => {
+        testHelper.getOnThisDayEvent.then((event) => {
+            const article = testHelper.onThisDayArticle(event)
+            const pretext = testHelper.pretext(testHelper.today, event)
+            const attachments = testHelper.attachments(article, pretext, color, articleKey)
+            const withActions = attachments.withActions
+            const [first] = withActions
+            
+            const [action] = first.actions
+
+            testSendAction(action)
+
+            done()
+        })
+    })
+
+    it('should create message attachments with cancel action', done => {
+        testHelper.getOnThisDayEvent.then((event) => {
+            const article = testHelper.onThisDayArticle(event)
+            const pretext = testHelper.pretext(testHelper.today, event)
+            const attachments = testHelper.attachments(article, pretext, color, articleKey)
+            const withActions = attachments.withActions
+            const [first] = withActions
+            
+            const [, action] = first.actions
+
+            testCancelAction(action)
+
+            done()
+        })
+    })
+
+    it('should create message attachments with actions and matching properties', done => {
+        testHelper.getOnThisDayEvent.then((event) => {
+            const article = testHelper.onThisDayArticle(event)
+            const pretext = testHelper.pretext(testHelper.today, event)
+            const attachments = testHelper.attachments(article, pretext, color, articleKey)
+            const withActions = attachments.withActions
+            const [first] = withActions
+
+            testMessageAttachmentsWithActionsShouldHaveMatchingProperties(first, pretext, color, articleKey)
+
+            done()
+        })
+    })
+
+    it('should create message attachments without actions', done => {
+        testHelper.getOnThisDayEvent.then((event) => {
+            const article = testHelper.onThisDayArticle(event)
+            const pretext = testHelper.pretext(testHelper.today, event)
+            const attachments = testHelper.attachments(article, pretext, color, articleKey)
+            const withoutActions = attachments.withoutActions
+
+            expect(withoutActions).to.not.be.null
+
+            done()
+        })
+    })
+
+    it('should create message attachments without actions and matching properties', done => {
+        testHelper.getOnThisDayEvent.then((event) => {
+            const article = testHelper.onThisDayArticle(event)
+            const pretext = testHelper.pretext(testHelper.today, event)
+            const attachments = testHelper.attachments(article, pretext, color, articleKey)
+            const withoutActions = attachments.withoutActions
+            
+            testMessageAttachmentsWithoutActionsShouldHaveMatchingProperties(withoutActions, pretext, color)
+
+            done()
+        })
     })
 })
