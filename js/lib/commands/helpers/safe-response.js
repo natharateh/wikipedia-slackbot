6e9d2966f358399d29e8f4b22d11f38a3b5310c6
Natalia Harateh <nambatee@users.noreply.github.com>
2018-10-19 07:37:04 -0400

    respond with post method

--- js/lib/commands/helpers/safe-response.js
+++ js/lib/commands/helpers/safe-response.js
@@ -8,6 +8,7 @@ export function respondImmediately(response) {
 
 export function respondWithDelay(uri, body) {
     const options = {
+        method: 'POST',
         uri,
         json: true,
         body,
